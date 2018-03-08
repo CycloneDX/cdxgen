edfd56839f2fb676ab5b3836d0fd17775aa69e51
Erlend Oftedal <erlend@oftedal.no>
2018-03-08 20:44:36 +0100

    Fixing license ++

--- index.js
+++ index.js
@@ -1,18 +1,15 @@
 const readInstalled = require("read-installed");
-const crypto = require("crypto");
-
-
 
 function createComponent(pkg) {
 	let component = { 
 		"@type" : "framework",
-		name: pkg.name,
-		version: pkg.version,
-		hashes: [],
+		name    : pkg.name,
+		version : pkg.version,
+		hashes  : [],
 		licenses: [
-			{license : { id: pkg.license } }
+			{ license : { id: pkg.license } }
 		],
-		purl: `pkg:npm/${pkg.name}@${pkg.version}`,
+		purl    : `pkg:npm/${pkg.name}@${pkg.version}`,
 		modified: false
 	};
 	if (pkg._shasum) {
@@ -24,19 +21,26 @@ function createComponent(pkg) {
 	return { component: component };
 }
 
-function createChild(name, value) {
+
+function createChild(name, value, depth) {
 	if (name == "value") return value;
-	if (Array.isArray(value)) return `<${name}>${value.map(js2Xml).join('')}</${name}>`;
+	if (Array.isArray(value)) return `<${name}>${value.map(v => js2Xml(v, depth + 1)).join('')}</${name}>`;
 	if (['boolean', 'string', 'number'].includes(typeof value)) return `<${name}>${value}</${name}>`;
 	throw new Error("Unexpected child");
 }
 
 
 function js2Xml(obj, depth) {
-	return Object.keys(obj).map(k => {
-		let attrs = Object.keys(obj[k]).filter(x => x.indexOf('@') == 0).map(x => ` ${x.slice(1)}="${obj[k][x]}"`).join('') || '';
-		let children = Object.keys(obj[k]).filter(x => x.indexOf('@') == -1).map(x => createChild(x, obj[k][x])).join('');		
-		return `<${k}${attrs}>${children}</${k}>`
+	return Object.keys(obj).map(key => {
+		let attrs = Object.keys(obj[key])
+			.filter(x => x.indexOf('@') == 0)
+			.map(x => ` ${x.slice(1)}="${obj[key][x]}"`)
+			.join('') || '';
+		let children = Object.keys(obj[key])
+			.filter(x => x.indexOf('@') == -1)
+			.map(x => createChild(x, obj[key][x], depth + 1))
+			.join('');		
+		return `<${key}${attrs}>${children}</${key}>`
 	}).join("\n");
 }
 
@@ -48,8 +52,6 @@ exports.createbom = path => readInstalled(path, (err, pkgInfo) => {
 		"@version": 1,
 		components: [ createComponent(pkgInfo) ]
 	}};
-	//console.log(result);
-	//js2Xml(result,0)
 	console.log(`<?xml version="1.0"?>\n${js2Xml(result,0)}`);
 });
 
