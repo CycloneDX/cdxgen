6d04c7fd7de810bc09198f8be84edf4bd89c3050
Patrick Dwyer <coderpatros@outlook.com>
2020-05-08 16:16:34 +1000

    Rename existing tests

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,13 +1,13 @@
 const bom = require("../index.js");
 
-test('no dependencies produces an empty BOM', done => {
+test('createbom produces an empty BOM', done => {
   bom.createbom(false, './tests/no-packages', {}, (err, bom) => {
     expect(bom).toMatchSnapshot();
     done();
   });
 });
 
-test('dependencies produces a BOM', done => {
+test('createbom produces a BOM without development dependencies', done => {
   bom.createbom(false, './tests/with-packages', {}, (err, bom) => {
     expect(bom).toMatchSnapshot();
     done();
