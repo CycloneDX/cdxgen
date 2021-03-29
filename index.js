34896499ae515ee685ab8113559717739d2b07b6
Prabhu Subramanian <prabhu@shiftleft.io>
2021-03-29 13:12:05 +0100

    Upgrade cyclonedx maven plugin to 2.3.0
    Upgrade cyclonedx maven plugin to 2.3.0 which no longer use jitpack

--- index.js
+++ index.js
@@ -412,7 +412,7 @@ const createJavaBom = async (
       "-DremoteRepositories=central::default::https://repo.maven.apache.org/maven2,jitpack::::https://jitpack.io",
       "-DrepoUrl=https://jitpack.io",
       "-Dartifact=com.github.everit-org.json-schema:org.everit.json.schema:1.12.1",
-      "org.cyclonedx:cyclonedx-maven-plugin:2.2.0:makeAggregateBom",
+      "org.cyclonedx:cyclonedx-maven-plugin:2.3.0:makeAggregateBom",
     ];
     // Support for passing additional settings and profile to maven
     if (process.env.MAVEN_EXTRA_OPTS) {
