60dfd5f574e5ff937ebc41c1442b2a2979f1d8c1
Soren Mathiasen <smo@tradeshift.com>
2021-06-03 08:25:56 +0200

    (fix) remove broken GitHub action

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-echo "Running scan in $GITHUB_WORKSPACE with FETCH_LICENSE=$FETCH_LICENSE"
-if [ ! -z "$SERVER_URL" ]
+echo "Running scan in $GITHUB_WORKSPACE with FETCH_LICENSE=${INPUT_FETCH_LICENSES}"
+if [ ! -z "${INPUT_FETCH_LICENSES}" ]
 then
     echo "Detecting license information from dependencies"
 fi
-FETCH_LICENSE=$FETCH_LICENSE node /app/bin/cdxgen $GITHUB_WORKSPACE -o  $GITHUB_WORKSPACE/bom.json
-if [ ! -z "$SERVER_URL" ]
+FETCH_LICENSE=${INPUT_FETCH_LICENSES} node /app/bin/cdxgen $GITHUB_WORKSPACE -o $GITHUB_WORKSPACE/bom.json
+if [ ! -z "${INPUT_SERVER_URL}" ]
 then
-    echo "Posting to $SERVER_URL"
-    curl -X POST -H "Authorization: Bearer $TOKEN" --data @/bom.json $SERVER_URL
+    echo "Posting to ${INPUT_SERVER_URL}"
+    cat ${GITHUB_WORKSPACE}/bom.json
+    curl -X POST -H 'Content-Type: application/json' -H "Authorization: Bearer ${INPUT_TOKEN}" --data @${GITHUB_WORKSPACE}/bom.json ${INPUT_SERVER_URL}
 fi
