13e07b764f86e7846f0ef8f679960053100e35e4
Soren Mathiasen <smo@tradeshift.com>
2021-06-03 08:17:53 +0200

    (fix) adding more debug information

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 echo "Running scan in $GITHUB_WORKSPACE with FETCH_LICENSE=$FETCH_LICENSE"
-ls -la $GITHUB_WORKSPACE
+if [ ! -z "$SERVER_URL" ]
+then
+    echo "Detecting license information from dependencies"
+fi
 FETCH_LICENSE=$FETCH_LICENSE node /app/bin/cdxgen $GITHUB_WORKSPACE -o  $GITHUB_WORKSPACE/bom.json
 if [ ! -z "$SERVER_URL" ]
 then
+    echo "Posting to $SERVER_URL"
     curl -X POST -H "Authorization: Bearer $TOKEN" --data @/bom.json $SERVER_URL
 fi
-ls -la $GITHUB_WORKSPACE
