84a95185934028f23456ef9f4b6a78e8bb56fe5c
Jan Kowalleck <jan.kowalleck@gmail.com>
2021-12-07 13:17:59 +0100

    describe test and contributing
    Signed-off-by: Jan Kowalleck <jan.kowalleck@gmail.com>

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,14 +1,12 @@
 const bomHelpers = require("../index.js");
-const fs = require("fs");
-const xmlFormat = require("prettify-xml");
-const xmlOptions = {indent: 4, newline: "\n"};
-const DomParser = require('@xmldom/xmldom').DOMParser;
 
 const timestamp = new Date("2020-01-01T01:00:00.000Z");
 const programVersion = "2.0.0";
 
 test('createbom produces an empty BOM', done => {
   bomHelpers.createbom("library", false, false, './tests/no-packages', {}, (err, bom) => {
+    expect(err).toBeFalsy();
+
     bom.metadata.timestamp = timestamp;
     bom.metadata.tools[0].version = programVersion;
     expect(bom.toXML()).toMatchSnapshot();
@@ -18,6 +16,8 @@ test('createbom produces an empty BOM', done => {
 
 test('createbom produces a BOM without development dependencies', done => {
   bomHelpers.createbom("library", false, true, './tests/with-packages', {}, (err, bom) => {
+    expect(err).toBeFalsy();
+
     bom.metadata.timestamp = timestamp;
     bom.metadata.tools[0].version = programVersion;
     expect(bom.toXML()).toMatchSnapshot();
@@ -27,6 +27,8 @@ test('createbom produces a BOM without development dependencies', done => {
 
 test('createbom produces a BOM with development dependencies', done => {
   bomHelpers.createbom("library", false, true, './tests/with-packages', { dev: true }, (err, bom) => {
+    expect(err).toBeFalsy();
+
     bom.metadata.timestamp = timestamp;
     bom.metadata.tools[0].version = programVersion;
     expect(bom.toXML()).toMatchSnapshot();
@@ -36,6 +38,8 @@ test('createbom produces a BOM with development dependencies', done => {
 
 test('createbom produces a BOM in JSON format', done => {
   bomHelpers.createbom("library", false, true, './tests/with-packages', {}, (err, bom) => {
+    expect(err).toBeFalsy();
+
     bom.metadata.timestamp = timestamp;
     bom.metadata.tools[0].version = programVersion;
     expect(bom.toJSON()).toMatchSnapshot();
@@ -45,6 +49,8 @@ test('createbom produces a BOM in JSON format', done => {
 
 test('createbom produces a BOM in JSON format that includes hashes from package-lock.json', done => {
   bomHelpers.createbom("library", false, true, './tests/with-lockfile-2', {}, (err, bom) => {
+    expect(err).toBeFalsy();
+
     bom.metadata.timestamp = timestamp;
     bom.metadata.tools[0].version = programVersion;
     expect(bom.toJSON()).toMatchSnapshot();
@@ -54,6 +60,8 @@ test('createbom produces a BOM in JSON format that includes hashes from package-
 
 test('createbom produces a BOM when no package-lock.json is present', done => {
   bomHelpers.createbom("library", false, true, './tests/no-lockfile', {}, (err, bom) => {
+    expect(err).toBeFalsy();
+
     bom.metadata.timestamp = timestamp;
     bom.metadata.tools[0].version = programVersion;
     expect(bom.toJSON()).toMatchSnapshot();
---
+++ tests/with-packages/README.md
@@ -0,0 +1,4 @@
+## Test: with packages
+
+this setup is intended to create reproducible results (SBoM).  
+It might install outdated, unmaintained or vulnerable components, for showcasing purposes.
