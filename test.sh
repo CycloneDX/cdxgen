04514a79dd8a905df58ddfd06785aac6a4a18a4d
Jan Kowalleck <jan.kowalleck@gmail.com>
2021-12-07 13:14:48 +0100

    modernized CI/CT
    Signed-off-by: Jan Kowalleck <jan.kowalleck@gmail.com>

--- test.sh
+++ test.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env bash
+## INTENDED TO BE RUN FROM PROJECT ROOT DIR
+set -ex
+
+## install project
 npm ci
-cd tests/with-packages
-npm ci
-cd ..
+
+## install testing-projects
+npm ci --prefix 'tests/with-packages'
+
+## run tests
 npm test
+
+## try to build project
+npm run build --if-present
