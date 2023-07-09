793e3018f6233f4b66c218d45bf74c33b0f931e4
malice00 <malice00@users.noreply.github.com>
2023-07-09 20:37:06 +0200

    Implemented handling of GRADLE_DEPENDENCY_TASK on v8.6.x (#387)
    Implemented handling of GRADLE_DEPENDENCY_TASK
    
    This fixes #384 by checking if EnvVar GRADLE_DEPENDENCY_TAK is set, and
    if so, use it (and possible arguments) instead of the default task
    'dependencies'
    
    Signed-off-by: Roland Asmann <roland.asmann@gmail.com>

--- index.js
+++ index.js
@@ -1244,17 +1244,26 @@ const createJavaBom = async (path, options) => {
     }
     if (gradleFiles && gradleFiles.length && options.installDeps) {
       let gradleCmd = utils.getGradleCommand(path, null);
+      const defaultDepTaskArgs = [
+        "-q",
+        "--console",
+        "plain",
+        "--build-cache"
+      ];
       allProjects.push(parentComponent);
+      let depTaskWithArgs = [
+        "dependencies"
+      ];
+      if (process.env.GRADLE_DEPENDENCY_TASK) {
+        depTaskWithArgs = process.env.GRADLE_DEPENDENCY_TASK.split(" ");
+      }
       for (let sp of allProjects) {
         let gradleDepArgs = [
           sp.purl === parentComponent.purl
-            ? "dependencies"
-            : `:${sp.name}:dependencies`,
-          "-q",
-          "--console",
-          "plain",
-          "--build-cache"
+            ? depTaskWithArgs[0]
+            : `:${sp.name}:${depTaskWithArgs[0]}`
         ];
+        gradleDepArgs = gradleDepArgs.concat(depTaskWithArgs.slice(1)).concat(defaultDepTaskArgs)
         // Support custom GRADLE_ARGS such as --configuration runtimeClassPath
         if (process.env.GRADLE_ARGS) {
           const addArgs = process.env.GRADLE_ARGS.split(" ");
