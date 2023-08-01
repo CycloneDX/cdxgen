476e5064063f309f9fee822ebff07b02d04e035e
Ajmal Kottilingal <90693406+ajmalab@users.noreply.github.com>
2023-08-01 08:14:09 +0100

    Fix/issue 421 (#422)
    * decode encoded purl
    
    Signed-off-by: Ajmal Kottilingal <ajmal.kottilingal@transferwise.com>
    
    * revert decoding purl
    
    Signed-off-by: Ajmal Kottilingal <ajmal.kottilingal@transferwise.com>
    
    * fix double encoding of purl
    
    Signed-off-by: Ajmal Kottilingal <ajmal.kottilingal@transferwise.com>
    
    * address comments
    
    Signed-off-by: Ajmal Kottilingal <ajmal.kottilingal@transferwise.com>
    
    * revert manual version bump
    
    Signed-off-by: Ajmal Kottilingal <ajmal.kottilingal@transferwise.com>
    
    * ignore dependency constraints in gradle output
    
    Signed-off-by: Ajmal Kottilingal <ajmal.kottilingal@transferwise.com>
    
    * remove test sbom
    
    Signed-off-by: Ajmal Kottilingal <ajmal.kottilingal@transferwise.com>
    
    ---------
    
    Signed-off-by: Ajmal Kottilingal <ajmal.kottilingal@transferwise.com>

--- utils.js
+++ utils.js
@@ -1447,6 +1447,14 @@ export const parseGradleDep = function (
       if (!rline) {
         continue;
       }
+      if ((
+        rline.startsWith("+--- ") || 
+        rline.startsWith("\\--- ")) && 
+        rline.includes("{strictly") && 
+        rline.includes("(c)")
+      ) {
+          continue;
+        }
       if (
         rline.trim() === "" ||
         rline.startsWith("+--- ") ||
