28b7d30b1a28e14e24670f58f4542923b82c0b37
Benji Visser <benji@093b.org>
2023-09-09 16:40:58 -0700

    ad scope required
    Signed-off-by: Benji Visser <benji@093b.org>

--- utils.js
+++ utils.js
@@ -4462,6 +4462,9 @@ export const parseCsProjAssetsData = async function (csProjData) {
         let pkg = {
             group: "",
             name: name,
+            // .NET ecosystem has no distinction between dev and prod dependencies
+            // like other ecosystems do, so we set scope to required for all packages
+            scope: "required",
             version: version,
             description: "",
             type: csProjData.targets[framework][rootDep].type,
