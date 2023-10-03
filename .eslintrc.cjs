33cd87a5bc1f8deeebf445b99363594ab4cf2d7c
Adam Setch <adam.setch@outlook.com>
2023-10-03 10:02:08 -0400

    build: add *.cjs to lint + pretty
    Signed-off-by: Adam Setch <adam.setch@outlook.com>

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,17 +1,16 @@
 module.exports = {
-    "env": {
-        "node": true,
-        "es2021": true
-    },
-    "plugins": ["prettier"],
-    "extends": ["eslint:recommended", "plugin:prettier/recommended"],
-    "overrides": [
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "rules": {
-        "prettier/prettier": "error",
-    }
-}
+  env: {
+    node: true,
+    es2021: true
+  },
+  plugins: ["prettier"],
+  extends: ["eslint:recommended", "plugin:prettier/recommended"],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module"
+  },
+  rules: {
+    "prettier/prettier": "error"
+  }
+};
