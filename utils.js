795796e6ab04bd81cf488598c76f7607248ef594
Robert Liias <robert.liias@napa.fi>
2023-10-19 12:12:41 +0300

    Revert nuget-semver, use coerce, refactor queryNuget
    Signed-off-by: Robert Liias <robert.liias@napa.fi>

--- utils.js
+++ utils.js
@@ -39,7 +39,6 @@ import {
   valid,
   parse
 } from "semver";
-import nugetSemVer from "@snyk/nuget-semver";
 import StreamZip from "node-stream-zip";
 import { parseEDNString } from "edn-data";
 import { PackageURL } from "packageurl-js";
@@ -7699,6 +7698,44 @@ async function getNugetUrl() {
 }
 
 async function queryNuget(p, NUGET_URL) {
+  function setLatestVersion(upper) {
+    // Handle special case for versions with more than 3 parts
+    if (upper.split(".").length > 3) {
+      let tmpVersionArray = upper.split("-")[0].split(".");
+      // Compromise for versions such as 1.2.3.0-alpha
+      // How to find latest proper release version?
+      if (
+        upper.split("-").length > 1 &&
+        Number(tmpVersionArray.slice(-1)) === 0
+      ) {
+        return upper;
+      } else if (upper.split("-").length > 1) {
+        tmpVersionArray[tmpVersionArray.length - 1] = (
+          Number(tmpVersionArray.slice(-1)) - 1
+        ).toString();
+      }
+      return tmpVersionArray.join(".");
+    } else {
+      const tmpVersion = parse(upper);
+      let version =
+        tmpVersion.major + "." + tmpVersion.minor + "." + tmpVersion.patch;
+      if (compare(version, upper) === 1) {
+        if (tmpVersion.patch > 0) {
+          version =
+            tmpVersion.major +
+            "." +
+            tmpVersion.minor +
+            "." +
+            (tmpVersion.patch - 1).toString();
+        }
+      }
+      return version;
+    }
+  }
+  // Coerce only when missing patch/minor version
+  function coerceUp(version) {
+    return version.split(".").length < 3 ? coerce(version).version : version;
+  }
   if (DEBUG_MODE) {
     console.log(`Querying nuget for ${p.name}`);
   }
@@ -7715,64 +7752,46 @@ async function queryNuget(p, NUGET_URL) {
   }
   if (items[0] && !items[0].items) {
     if (!p.version || p.version === "0.0.0" || p.version === "latest") {
-      const tmpVersion = parse(res.body.items[res.body.items.length - 1].upper);
-      np.version =
-        tmpVersion.major + "." + tmpVersion.minor + "." + tmpVersion.patch;
-      if (
-        compare(np.version, res.body.items[res.body.items.length - 1].upper) ===
-        1
-      ) {
-        if (tmpVersion.patch > 0) {
-          np.version =
-            tmpVersion.major +
-            "." +
-            tmpVersion.minor +
-            "." +
-            (tmpVersion.patch - 1).toString();
-        }
-      }
+      let upper = items[items.length - 1].upper;
+      np.version = setLatestVersion(upper);
     }
     for (const item of items) {
-      // if (!p.version || p.version === "0.0.0" || p.version === "latest") {
-      //   const tmpVersion = parse(res.body.items[res.body.items.length - 1].upper);
-      //   np.version = tmpVersion.major + "." + tmpVersion.minor + "." + tmpVersion.patch;
-      //
-      // }
       if (np.version) {
-        let lower = nugetSemVer.compare(item.lower, np.version);
-        let upper = nugetSemVer.compare(item.upper, np.version);
+        let lower = compare(coerce(item.lower), coerce(np.version));
+        let upper = compare(coerce(item.upper), coerce(np.version));
         if (lower !== 1 && upper !== -1) {
           res = await cdxgenAgent.get(item["@id"], { responseType: "json" });
-          newBody.push(
-            res.body.items
-              .reverse()
-              .filter(
-                (i) => i.catalogEntry && i.catalogEntry.version === np.version
-              )
-          );
-          break;
+          for (const i of res.body.items.reverse()) {
+            if (
+              i.catalogEntry &&
+              i.catalogEntry.version === coerceUp(np.version)
+            ) {
+              newBody.push(i);
+              return [np, newBody];
+            }
+          }
         }
       }
     }
   } else {
     if (!p.version || p.version === "0.0.0" || p.version === "latest") {
-      const tmpVersion = parse(res.body.items[res.body.items.length - 1].upper);
-      np.version =
-        tmpVersion.major + "." + tmpVersion.minor + "." + tmpVersion.patch;
+      let upper = items[items.length - 1].upper;
+      np.version = setLatestVersion(upper);
     }
     if (np.version) {
       for (const item of items) {
-        let lower = nugetSemVer.compare(item.lower, np.version);
-        let upper = nugetSemVer.compare(item.upper, np.version);
+        let lower = compare(coerce(item.lower), coerce(np.version));
+        let upper = compare(coerce(item.upper), coerce(np.version));
         if (lower !== 1 && upper !== -1) {
-          newBody.push(
-            item.items
-              .reverse()
-              .filter(
-                (i) => i.catalogEntry && i.catalogEntry.version === np.version
-              )
-          );
-          break;
+          for (const i of item.items.reverse()) {
+            if (
+              i.catalogEntry &&
+              i.catalogEntry.version === coerceUp(np.version)
+            ) {
+              newBody.push(i);
+              return [np, newBody];
+            }
+          }
         }
       }
     }
@@ -7823,8 +7842,8 @@ export const getNugetMetadata = async function (
           depRepList[oldRef] = p["bom-ref"];
           p.version = np.version;
         }
-        if (newBody && newBody[0].length > 0) {
-          body = newBody[0][0];
+        if (newBody && newBody.length > 0) {
+          body = newBody[0];
         }
         if (body) {
           metadata_cache[cacheKey] = body;
