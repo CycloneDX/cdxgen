56132d73fa5cef205cdc5e6581c69d3c26a1df05
gbennett <gbennett@squarespace.com>
2023-11-28 11:47:58 -0800

    linting
    Signed-off-by: gbennett <gbennett@squarespace.com>

--- utils.js
+++ utils.js
@@ -4682,11 +4682,11 @@ export const parseOpenapiSpecData = function (oaData) {
   }
 
   const name =
-      oaData.info && oaData.info.title
+    oaData.info && oaData.info.title
       ? oaData.info.title.replace(/ /g, "-")
       : "default-name";
   const version =
-      oaData.info && oaData.info.version ? oaData.info.version : "latest";
+    oaData.info && oaData.info.version ? oaData.info.version : "latest";
   const aservice = {
     "bom-ref": `urn:service:${name}:${version}`,
     name,
