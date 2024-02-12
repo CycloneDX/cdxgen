8bb1c15ad59221fa65147a32003a54a4778b82e5
Prabhu Subramanian <prabhu@appthreat.com>
2024-02-12 19:46:40 +0000

    Reduce sbom size
    Signed-off-by: Prabhu Subramanian <prabhu@appthreat.com>

--- bin/cdxgen.js
+++ bin/cdxgen.js
@@ -404,7 +404,15 @@ const checkPermissions = (filePath) => {
         fs.writeFileSync(jsonFile, bomNSData.bomJson);
         jsonPayload = bomNSData.bomJson;
       } else {
-        jsonPayload = JSON.stringify(bomNSData.bomJson, null, 2);
+        jsonPayload = JSON.stringify(
+          bomNSData.bomJson,
+          null,
+          options.deep ||
+            ["os", "docker", "universal"].includes(options.projectType) ||
+            process.env.CI
+            ? null
+            : 2
+        );
         fs.writeFileSync(jsonFile, jsonPayload);
       }
       if (
