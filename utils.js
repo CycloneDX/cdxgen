eee6a0f8f7750e1a51919cbc573c2e838458ead8
Florian Hager <florian.hager@mobile-pocket.com>
2024-02-19 14:51:09 +0100

    run lint
    Signed-off-by: Florian Hager <florian.hager@mobile-pocket.com>

--- utils.js
+++ utils.js
@@ -6723,19 +6723,19 @@ const purlFromUrlString = (type, repoUrl, version) => {
   let namespace = "",
     name;
   if (repoUrl && repoUrl.startsWith("http")) {
-    let url = new URL(repoUrl);
-    let pathnameParts = url.pathname.split('/');
-    let pathnameLastElement = pathnameParts.pop(); // pop() returns last element and removes it from pathnameParts
-    name = pathnameLastElement.replace('.git','');
-    let urlpath = pathnameParts.join('/');
+    const url = new URL(repoUrl);
+    const pathnameParts = url.pathname.split("/");
+    const pathnameLastElement = pathnameParts.pop(); // pop() returns last element and removes it from pathnameParts
+    name = pathnameLastElement.replace(".git", "");
+    const urlpath = pathnameParts.join("/");
     namespace = url.hostname + urlpath;
   } else if (repoUrl && repoUrl.startsWith("git@")) {
-    let parts = repoUrl.split(':')
-    let hostname = parts[0].split('@')[1];
-    let pathnameParts = parts[1].split('/');
-    let pathnameLastElement = pathnameParts.pop();
-    name = pathnameLastElement.replace('.git','');
-    let urlpath = pathnameParts.join('/');
+    const parts = repoUrl.split(":");
+    const hostname = parts[0].split("@")[1];
+    const pathnameParts = parts[1].split("/");
+    const pathnameLastElement = pathnameParts.pop();
+    name = pathnameLastElement.replace(".git", "");
+    const urlpath = pathnameParts.join("/");
     namespace = hostname + ":" + urlpath;
   } else if (repoUrl && repoUrl.startsWith("/")) {
     const parts = repoUrl.split("/");
