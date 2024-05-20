82c66677d7b2681bac4ec6a8d1ad6681e7d5c195
g-kaz <55018447+g-kaz@users.noreply.github.com>
2024-05-20 21:40:09 +0500

    Move before check pnpmLockFiles + Add check lock files for yarn and pnpm
    Signed-off-by: g-kaz <55018447+g-kaz@users.noreply.github.com>

--- index.js
+++ index.js
@@ -2150,6 +2150,56 @@ export async function createNodejsBom(path, options) {
       }
     }
   }
+  const pkgJsonLockFile = getAllFiles(path, "package-lock.json", options);
+  const pkgJsonFile = getAllFiles(path, "package.json", options);
+  const yarnLockFile = getAllFiles(path, "yarn.lock", options);
+  const pnpmLockFile = getAllFiles(path, "pnpm-lock.yaml", options);
+  if (
+    (pkgJsonLockFile?.length === 0 ||
+      yarnLockFile?.length === 0 ||
+      pnpmLockFile?.length === 0) &&
+    pkgJsonFile?.length === 1 &&
+    options.installDeps
+  ) {
+    let pkgMgr = "npm";
+    const supPkgMgrs = ["npm", "yarn", "yarnpkg", "pnpm", "pnpx"];
+    const pkgData = JSON.parse(readFileSync(`${path}/package.json`, "utf8"));
+    const mgrData = pkgData.packageManager;
+    let mgr = "";
+    if (mgrData) {
+      mgr = mgrData.split("@")[0];
+    }
+    if (supPkgMgrs.includes(mgr)) {
+      pkgMgr = mgr;
+    }
+    console.log(`Executing '${pkgMgr} install' in`, path);
+    const result = spawnSync(pkgMgr, ["install"], {
+      cwd: path,
+      encoding: "utf-8",
+    });
+    if (result.status !== 0 || result.error) {
+      console.error(
+        `${pkgMgr} install has failed. Check if ${pkgMgr} is installed and available in PATH.`,
+      );
+      console.log(result.error, result.stderr);
+      options.failOnError && process.exit(1);
+    }
+    pkgLockFiles = getAllFiles(
+      path,
+      `${options.multiProject ? "**/" : ""}package-lock.json`,
+      options,
+    );
+    pnpmLockFiles = getAllFiles(
+      path,
+      `${options.multiProject ? "**/" : ""}pnpm-lock.yaml`,
+      options,
+    );
+    yarnLockFiles = getAllFiles(
+      path,
+      `${options.multiProject ? "**/" : ""}yarn.lock`,
+      options,
+    );
+  }
   if (pnpmLockFiles?.length) {
     manifestFiles = manifestFiles.concat(pnpmLockFiles);
     for (const f of pnpmLockFiles) {
@@ -2206,42 +2256,6 @@ export async function createNodejsBom(path, options) {
         );
       }
     }
-  }
-  const pkgJsonLockFile = getAllFiles(path, "package-lock.json", options);
-  const pkgJsonFile = getAllFiles(path, "package.json", options);
-  if (
-    pkgJsonLockFile?.length === 0 &&
-    pkgJsonFile?.length === 1 &&
-    options.installDeps
-  ) {
-    let pkgMgr = "npm";
-    const supPkgMgrs = ["npm", "yarn", "yarnpkg", "pnpm", "pnpx"];
-    const pkgData = JSON.parse(readFileSync(`${path}/package.json`, "utf8"));
-    const mgrData = pkgData.packageManager;
-    let mgr = "";
-    if (mgrData) {
-      mgr = mgrData.split("@")[0];
-    }
-    if (supPkgMgrs.includes(mgr)) {
-      pkgMgr = mgr;
-    }
-    console.log(`Executing '${pkgMgr} install' in`, path);
-    const result = spawnSync(pkgMgr, ["install"], {
-      cwd: path,
-      encoding: "utf-8",
-    });
-    if (result.status !== 0 || result.error) {
-      console.error(
-        `${pkgMgr} install has failed. Check if ${pkgMgr} is installed and available in PATH.`,
-      );
-      console.log(result.error, result.stderr);
-      options.failOnError && process.exit(1);
-    }
-    pkgLockFiles = getAllFiles(
-      path,
-      `${options.multiProject ? "**/" : ""}package-lock.json`,
-      options,
-    );
   }
   if (pkgLockFiles?.length) {
     manifestFiles = manifestFiles.concat(pkgLockFiles);
