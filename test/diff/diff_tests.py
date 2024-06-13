ab1f7163dd1fdef3932b2eee3584fd5b5f5f36aa
Caroline Russell <caroline@appthreat.dev>
2024-06-13 08:39:13 -0400

    Update snapshot tests to integrate new custom-json-diff functionality. (#1157)
    * Update snapshot tests to integrate new custom-json-diff functionality.
    
    Signed-off-by: Caroline Russell <caroline@appthreat.dev>
    
    * Resolve workflow pnpm issue, add includes for snapshot tests.
    
    Signed-off-by: Caroline Russell <caroline@appthreat.dev>
    
    Troubleshoot pnpm issue.
    
    Signed-off-by: Caroline Russell <caroline@appthreat.dev>
    
    * Update netty commit.
    
    Signed-off-by: Caroline Russell <caroline@appthreat.dev>
    
    * Use package.json pnpm version, remove unneeded atom installation.
    
    Signed-off-by: Caroline Russell <caroline@appthreat.dev>
    
    ---------
    
    Signed-off-by: Caroline Russell <caroline@appthreat.dev>

--- test/diff/diff_tests.py
+++ test/diff/diff_tests.py
@@ -1,34 +1,45 @@
 import csv
 import json
 import os
-from pathlib import Path
 
-from custom_json_diff.custom_diff import compare_dicts, get_diffs
+from custom_json_diff.custom_diff import compare_dicts, perform_bom_diff, report_results
+from custom_json_diff.custom_diff_classes import Options
+
 
 with open('/home/runner/work/cdxgen/cdxgen/test/diff/repos.csv', 'r', encoding='utf-8') as f:
     reader = csv.DictReader(f)
     repo_data = list(reader)
 
-Failed = False
 failed_diffs = {}
 
 for i in repo_data:
     bom_file = f'{i["project"]}-bom.json'
-    bom_1 = Path("/home/runner/work/samples", bom_file)
-    bom_2 = Path("/home/runner/work/cdxgen-samples", bom_file)
+    bom_1 = f"/home/runner/work/samples/{bom_file}"
+    bom_2 = f"/home/runner/work/cdxgen-samples/{bom_file}"
+    exclude = []
+    include = ["properties", "evidence", "licenses"]
+    options = Options(
+        allow_new_versions=True,
+        allow_new_data=True,
+        bom_diff=True,
+        include=include,
+        exclude=exclude,
+        file_1=bom_1,
+        file_2=bom_2,
+        output=f'/home/runner/work/cdxgen-samples/{i["project"]}-diff.json'
+    )
     if not os.path.exists(bom_1):
         print(f'{bom_file} does not exist in cdxgen-samples repository.')
         failed_diffs[i["project"]] = f'{bom_file} does not exist in cdxgen-samples repository.'
         continue
-    result, j1, j2 = compare_dicts(bom_1, bom_2, preset="cdxgen")
-    if result == 0:
-        print(f'{i["project"]} BOM passed.')
-    else:
-        print(f'{i["project"]} BOM failed.')
-        Failed = True
-        diffs = get_diffs(bom_1, bom_2, j1, j2)
-        failed_diffs[i["project"]] = diffs
+    result, j1, j2 = compare_dicts(options)
+
+    if result != 0:
+        print(f"{i['project']} failed.")
+        result_summary = perform_bom_diff(j1, j2)
+        failed_diffs[i["project"]] = result_summary
+        report_results(result, result_summary, options, j1, j2)
 
 if failed_diffs:
     with open('/home/runner/work/cdxgen-samples/diffs.json', 'w') as f:
-        json.dump(failed_diffs, f, indent=2)
+        json.dump(failed_diffs, f)
