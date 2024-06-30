4aaf681d625cd522d3ebfdfc8db8a79b9a4b6fb3
Ajmal Kottilingal <ajmal.kottilingal@transferwise.com>
2024-06-30 20:50:46 +0100

    remove log statement
    Signed-off-by: Ajmal Kottilingal <ajmal.kottilingal@transferwise.com>

--- utils.js
+++ utils.js
@@ -2633,7 +2633,7 @@ export function executeParallelGradleProperties(dir, rootPath, allProjectsStr) {
   for (const spstr of allProjectsStr) {
     parallelPropTaskArgs.push(`${spstr}:properties`);
   }
-  
+
   let gradlePropertiesArgs = [
     "--console",
     "plain",
@@ -9094,7 +9094,6 @@ export function splitOutputByGradleProjects(rawOutput) {
       outputSplitBySubprojects.set(currentProjectName, subProjectOut);
     }
   }
-  console.log(JSON.stringify(outputSplitBySubprojects))
   return outputSplitBySubprojects;
 }
 /**
