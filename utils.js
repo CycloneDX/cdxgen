82a55b59979831b3ff5e637e2e400534f7abab27
Maxime Robert <maxime.robert@smile.fr>
2024-08-26 18:43:33 +0200

    Make sure composer.lock dependency package version is a string (#1325)
    composer.lock files may have a dependency package version as a number instead of a string (see: https://github.com/google/osv-scanner/issues/1138). In that case `new PackageURL(...)` throws the following error: Error: Invalid purl: "versions" argument must be a string.
    
    Signed-off-by: Maxime Robert <maxime.robert@smile.fr>

--- utils.js
+++ utils.js
@@ -7874,7 +7874,7 @@ export function parseComposerLock(pkgLockFile, rootRequires) {
             "composer",
             group,
             name,
-            pkg.version,
+            pkg.version?.toString(),
             null,
             null,
           ).toString();
@@ -7883,7 +7883,7 @@ export function parseComposerLock(pkgLockFile, rootRequires) {
             name: name,
             purl,
             "bom-ref": decodeURIComponent(purl),
-            version: pkg.version,
+            version: pkg.version?.toString(),
             repository: pkg.source,
             license: pkg.license,
             description: pkg.description,
