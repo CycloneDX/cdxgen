935c099453009250ae1bef05d7e98c7c36472d1d
Caroline Russell <caroline@appthreat.dev>
2024-11-18 16:57:37 -0500

    Remove sdkman step.
    Signed-off-by: Caroline Russell <caroline@appthreat.dev>

--- test/diff/generate.py
+++ test/diff/generate.py
@@ -263,8 +263,8 @@ def generate(args):
     if not args.debug_cmds:
         check_dirs(args.skip_clone, args.clone_dir, args.output_dir)
 
-    if not args.skip_build:
-        run_pre_builds(repo_data, args.output_dir, args.debug_cmds)
+    # if not args.skip_build:
+    #     run_pre_builds(repo_data, args.output_dir, args.debug_cmds)
 
     commands = ""
     cdxgen_log = os.getenv("CDXGEN_LOG")
@@ -447,8 +447,9 @@ def write_script_file(file_path, commands, debug_cmds):
     Returns:
         None
     """
-    sdkman_path = Path.joinpath(Path('$SDKMAN_DIR'), 'bin', 'sdkman-init.sh')
-    cmds = f'#!/usr/bin/bash\nsource {sdkman_path}\n\n{commands}'
+    # sdkman_path = Path.joinpath(Path('$SDKMAN_DIR'), 'bin', 'sdkman-init.sh')
+    # cmds = f'#!/usr/bin/bash\nsource {sdkman_path}\n\n{commands}'
+    cmds = f'#!/usr/bin/bash\n\n{commands}'
     file_write(str(file_path), cmds, success_msg=f"Wrote script to {file_path}.")
     if debug_cmds:
         print(commands)
