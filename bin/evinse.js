db5ecc199cd6494177e50a427d79498d280ddb76
Prabhu Subramanian <prabhu@appthreat.com>
2025-01-26 16:12:33 +0000

    Secure mode WIP
    Signed-off-by: Prabhu Subramanian <prabhu@appthreat.com>

--- bin/evinse.js
+++ bin/evinse.js
@@ -162,6 +162,10 @@ const evinseArt = `
 ╚══════╝  ╚═══╝  ╚═╝╚═╝  ╚═══╝╚══════╝╚══════╝
 `;
 
+if (process.env?.CDXGEN_NODE_OPTIONS) {
+  process.env.NODE_OPTIONS = `${process.env.NODE_OPTIONS || ""} ${process.env.CDXGEN_NODE_OPTIONS}`;
+}
+
 console.log(evinseArt);
 (async () => {
   // First, prepare the database by cataloging jars and other libraries
