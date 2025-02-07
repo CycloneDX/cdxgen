16b4737ada90531ef5b090dce39993b6d4a64c6d
prabhu <7842+prabhu@users.noreply.github.com>
2025-02-07 21:24:48 +0000

    cdx1 (#1624)
    * cdx1 mlx
    
    Signed-off-by: Prabhu Subramanian <prabhu@appthreat.com>
    
    * Ignore contrib in biome
    
    Signed-off-by: Prabhu Subramanian <prabhu@appthreat.com>
    
    * Fix tests
    
    Signed-off-by: Prabhu Subramanian <prabhu@appthreat.com>
    
    ---------
    
    Signed-off-by: Prabhu Subramanian <prabhu@appthreat.com>

---
+++ contrib/fine-tuning/prepare.js
@@ -0,0 +1,34 @@
+import { mkdirSync, readFileSync, writeFileSync } from "node:fs";
+import { join } from "node:path";
+import process from "node:process";
+
+import { dirNameStr, getAllFiles } from "../../lib/helpers/utils.js";
+const jsonlFiles = getAllFiles(dirNameStr, "**/*.jsonl", {
+  exclude: ["**/{train, valid}.jsonl"],
+});
+let datasetDir = "dataset";
+const argv = process.argv.slice(2);
+if (argv.length > 1) {
+  datasetDir = argv[1];
+}
+
+const TRAIN_FILE = join(datasetDir, "train.jsonl");
+const VALID_FILE = join(datasetDir, "valid.jsonl");
+const trainData = [];
+const validData = [];
+
+for (const jf of jsonlFiles) {
+  const lines = readFileSync(jf, "utf-8");
+  trainData.push(lines);
+  if (jf.includes("readme") || jf.includes("cdxgen") || jf.includes("cli")) {
+    validData.push(lines);
+  }
+}
+
+mkdirSync(datasetDir, { recursive: true });
+if (trainData.length) {
+  writeFileSync(TRAIN_FILE, trainData.join("\n"));
+}
+if (validData.length) {
+  writeFileSync(VALID_FILE, validData.join("\n"));
+}
---
+++ contrib/fine-tuning/upload-hf.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+set -e
+export HF_HUB_ENABLE_HF_TRANSFER=0
+HF_ORG=CycloneDX
+TUNING_TOOL=mlx
+TOOL_BASE_MODEL=cdx1
+FUSED_MODEL=${HF_ORG}/${TOOL_BASE_MODEL}-${TUNING_TOOL}
+FUSED_GGUF_MODEL=${HF_ORG}/${TOOL_BASE_MODEL}-gguf
+QUANT_MODEL_8BIT=${HF_ORG}/${TOOL_BASE_MODEL}-${TUNING_TOOL}-8bit
+QUANT_MODEL_6BIT=${HF_ORG}/${TOOL_BASE_MODEL}-${TUNING_TOOL}-6bit
+QUANT_MODEL_4BIT=${HF_ORG}/${TOOL_BASE_MODEL}-${TUNING_TOOL}-4bit
+
+huggingface-cli whoami
+
+echo "Uploading datasets"
+huggingface-cli upload --repo-type dataset CycloneDX/cdx-docs ./cdxgen-docs cdxgen-docs
+huggingface-cli upload --repo-type dataset CycloneDX/cdx-docs ./guides guides
+
+echo "Uploading models. Please wait ..."
+huggingface-cli upload --repo-type model ${QUANT_MODEL_8BIT} ./${QUANT_MODEL_8BIT} .
+huggingface-cli upload --repo-type model ${QUANT_MODEL_6BIT} ./${QUANT_MODEL_6BIT} .
+huggingface-cli upload --repo-type model ${QUANT_MODEL_4BIT} ./${QUANT_MODEL_4BIT} .
+
+huggingface-cli upload --repo-type model ${FUSED_MODEL} ./${FUSED_MODEL} .
+huggingface-cli upload --repo-type model ${FUSED_GGUF_MODEL} ./${FUSED_GGUF_MODEL} .
