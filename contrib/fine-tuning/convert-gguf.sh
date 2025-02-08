943afde72ede3d0fae46b9b93a9f3182ddbe4e73
Prabhu Subramanian <prabhu@appthreat.com>
2025-02-08 14:02:36 +0000

    semantics data
    Signed-off-by: Prabhu Subramanian <prabhu@appthreat.com>

--- contrib/fine-tuning/convert-gguf.sh
+++ contrib/fine-tuning/convert-gguf.sh
@@ -13,22 +13,23 @@ TOOL_BASE_MODEL=cdx1
 LLAMA_CPP_PATH=/Volumes/Work/sandbox/llama.cpp
 cd $LLAMA_CPP_PATH
 CDXGEN_FT_PATH=/Volumes/Work/CycloneDX/cdxgen/contrib/fine-tuning
-GGUF_MODEL_Q8_0=${CDXGEN_FT_PATH}/${HF_ORG}/${TOOL_BASE_MODEL}-gguf-Q8_0-GGUF
+GGUF_MODEL_Q8_0_NAME=${HF_ORG}/${TOOL_BASE_MODEL}-gguf-Q8_0-GGUF
+GGUF_MODEL_Q8_0_PATH=${CDXGEN_FT_PATH}/${HF_ORG}/${TOOL_BASE_MODEL}-gguf-Q8_0-GGUF
 FUSED_MODEL=${CDXGEN_FT_PATH}/${HF_ORG}/${TOOL_BASE_MODEL}-${TUNING_TOOL}
 
-rm -rf ${GGUF_MODEL_Q8_0}
-mkdir -p ${GGUF_MODEL_Q8_0}
-python convert_hf_to_gguf.py --outtype q8_0 --outfile ${CDXGEN_FT_PATH}/${HF_ORG}/${TOOL_BASE_MODEL}-gguf-Q8_0-GGUF/${TOOL_BASE_MODEL}-gguf-q8_0.gguf --model-name ${GGUF_MODEL_Q8_0} ${FUSED_MODEL}
+rm -rf ${GGUF_MODEL_Q8_0_PATH}
+mkdir -p ${GGUF_MODEL_Q8_0_PATH}
+python convert_hf_to_gguf.py --outtype q8_0 --outfile ${CDXGEN_FT_PATH}/${HF_ORG}/${TOOL_BASE_MODEL}-gguf-Q8_0-GGUF/${TOOL_BASE_MODEL}-gguf-q8_0.gguf --model-name ${GGUF_MODEL_Q8_0_NAME} ${FUSED_MODEL}
 
-cp ${CDXGEN_FT_PATH}/Modelfile ${GGUF_MODEL_Q8_0}/
-# cp ${FUSED_MODEL}/*.json ${FUSED_MODEL}/merges.txt ${GGUF_MODEL_Q8_0}/
+cp ${CDXGEN_FT_PATH}/Modelfile ${GGUF_MODEL_Q8_0_PATH}/
+# cp ${FUSED_MODEL}/*.json ${FUSED_MODEL}/merges.txt ${GGUF_MODEL_Q8_0_PATH}/
 
 ### Testing with ollama
-# cd ${GGUF_MODEL_Q8_0}
+# cd ${GGUF_MODEL_Q8_0_PATH}
 # ollama create cdx1-gguf -f Modelfile
 # ollama show cdx1-gguf
-# ollama run cdx1-gguf 
+# ollama run cdx1-gguf "tell me about cdxgen"
 
 export HF_HUB_ENABLE_HF_TRANSFER=0
 huggingface-cli whoami
-huggingface-cli upload --quiet --repo-type model ${GGUF_MODEL_Q8_0} ./${GGUF_MODEL_Q8_0} .
+huggingface-cli upload --quiet --repo-type model ${GGUF_MODEL_Q8_0_NAME} ${GGUF_MODEL_Q8_0_PATH} .
