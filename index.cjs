d88c8a4fc518e4f76b2184f07b5f67057a5aa490
Jonathan Heller <jonathan.heller@backslash.security>
2025-03-30 15:41:31 +0300

    fix lint issues
    Signed-off-by: Jonathan Heller <jonathan.heller@backslash.security>

--- index.cjs
+++ index.cjs
@@ -1,10 +1,15 @@
 // this file is a wrapper of ./lib/cli/index.js that can be used by commonjs projects importing this module
 // that prefer to use require instead of await import()
-const importPromise = import('./lib/cli/index.js');
+const importPromise = import("./lib/cli/index.js");
 
-module.exports = new Proxy({}, {
-    get: (_, prop) => async (...args) => {
-    const mod = await importPromise;
-    return typeof mod[prop] === "function" ? mod[prop](...args) : mod[prop];
-  }
-});
+module.exports = new Proxy(
+  {},
+  {
+    get:
+      (_, prop) =>
+      async (...args) => {
+        const mod = await importPromise;
+        return typeof mod[prop] === "function" ? mod[prop](...args) : mod[prop];
+      },
+  },
+);
