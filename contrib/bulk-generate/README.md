14ca2db14a451017f1a4e38eaf3057b3a3bb0d8b
bandhan-majumder <bandhanmajumder16@gmail.com>
2025-06-17 18:52:43 +0530

    Add script for checking docker image existance
    Signed-off-by: bandhan-majumder <bandhanmajumder16@gmail.com>

--- contrib/bulk-generate/README.md
+++ contrib/bulk-generate/README.md
@@ -1,16 +1,33 @@
 # Introduction
 
-This is a script to generate SBOMs for multiple git repos using cdxgen container images.
+These are two different scripts to generate SBOMs for multiple git repos using cdxgen container images and testing cdxgen container images existance.
 
-## Usage
+## Generate SBOMs script
+
+### Usage
 
 ```shell
 node index.js <csv file> <output directory>
 ```
 
-## Example csv file
+### Example csv file
 
 ```
 project,link,commit,image,language,cdxgen_vars
 astro,https://github.com/withastro/astro.git,9d6bcdb88fcb9df0c5c70e2b591bcf962ce55f63,ghcr.io/cyclonedx/cdxgen-node20:v11,js,,
+```
+
+## Image existance script
+
+### Usage
+
+```shell
+node image-avail.js <csv file>
+```
+
+### Example csv file
+
+```
+owner,repo,tag
+cyclonedx,cdxgen,master,,
 ```
---
+++ contrib/bulk-generate/image-avail.js
@@ -0,0 +1,58 @@
+#!/usr/bin/env node
+
+import { spawnSync } from "node:child_process";
+import { readFileSync } from "node:fs";
+import process from "node:process";
+
+const DOCKER_CMD = process.env.DOCKER_CMD || "docker";
+
+function checkImageExists(owner, repo, tag) {
+  const image = `ghcr.io/${owner}/${repo}:${tag}`;
+  const args = ["manifest", "inspect", image];
+  const result = spawnSync(DOCKER_CMD, args, {
+    encoding: "utf-8",
+    stdio: "ignore",
+    env: process.env,
+  });
+  if (result.status === 0) {
+    console.log(`${image}: Exists`);
+    return true;
+  } else {
+    console.log(`${image}: Not found`);
+    return false;
+  }
+}
+
+function readcsv(acsv) {
+  const argsList = [];
+  const csvLines = readFileSync(acsv, { encoding: "utf-8" })
+    .split("\n")
+    .filter(Boolean)
+    .slice(1);
+  for (const aline of csvLines) {
+    const values = aline.split(/[,|]/);
+    if (values.length < 3) continue;
+    argsList.push({
+      owner: values[0].trim(),
+      repo: values[1].trim(),
+      tag: values[2].trim(),
+    });
+  }
+  return argsList;
+}
+
+function main(argvs) {
+  if (argvs.length !== 1) {
+    console.log("USAGE: node image-avail.js <csv file>");
+    process.exit(1);
+  }
+  const imagesList = readcsv(argvs[0]);
+  for (const imageArgs of imagesList) {
+    if (!imageArgs?.owner || !imageArgs?.repo || !imageArgs?.tag) {
+      continue;
+    }
+    checkImageExists(imageArgs.owner, imageArgs.repo, imageArgs.tag);
+  }
+}
+
+main(process.argv.slice(2)); 
