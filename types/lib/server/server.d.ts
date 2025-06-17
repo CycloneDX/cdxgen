be8cffbbccbc6ce5f0672d605effa2bf6e263052
Prabhu Subramanian <prabhu@appthreat.com>
2025-06-17 23:42:03 +0100

    Restrict the type of values that can be posted
    Signed-off-by: Prabhu Subramanian <prabhu@appthreat.com>

--- types/lib/server/server.d.ts
+++ types/lib/server/server.d.ts
@@ -1,3 +1,14 @@
+export function isAllowedHost(hostname: any): any;
+export function isAllowedPath(p: any): any;
+/**
+ * Method to safely parse value passed via the query string or body.
+ *
+ * @param {string|number|Array<string|number>} raw
+ * @returns {string|number|boolean|Array<string|number|boolean>}
+ * @throws {TypeError} if raw (or any array element) isn’t string or number
+ */
+export function parseValue(raw: string | number | Array<string | number>): string | number | boolean | Array<string | number | boolean>;
+export function parseQueryString(q: any, body?: {}, options?: {}): {};
 export function configureServer(cdxgenServer: any): void;
 export function start(options: any): void;
 //# sourceMappingURL=server.d.ts.map
