7b160165a00790b94f9d6eca954e8fa955f2aff6
roshan <58213083+youhaveme9@users.noreply.github.com>
2025-08-07 14:59:33 +0530

    [feat] Added nix flake support (#2138)
    * [feat] Added nix flake support
    
    Signed-off-by: Roshan Kumar <roshaen09@gmail.com>
    
    * [fix] Fixed linting errors
    
    Signed-off-by: Roshan Kumar <roshaen09@gmail.com>
    
    * [tests] Added unit tests for flake support
    
    Signed-off-by: Roshan Kumar <roshaen09@gmail.com>
    
    * [fix] Fixed linting errors
    
    Signed-off-by: Roshan Kumar <roshaen09@gmail.com>
    
    * [fix] Refactored flake parsing logic
    
    Signed-off-by: Roshan Kumar <roshaen09@gmail.com>
    
    ---------
    
    Signed-off-by: Roshan Kumar <roshaen09@gmail.com>

---
+++ test/data/test-flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/release-23.11";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, rust-overlay }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            nodejs
+            python3
+            rust-bin.stable.latest.default
+          ];
+        };
+      });
+} 
