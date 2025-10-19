a8f8469cfa70ed046953d34b049830117afc32ac
prabhu <7842+prabhu@users.noreply.github.com>
2025-10-19 14:52:39 +0100

    yarn workspace improvements (#2504)
    * Improved yarn workspace source tracking
    
    Signed-off-by: Prabhu Subramanian <prabhu@appthreat.com>
    
    * Tests
    
    Signed-off-by: Prabhu Subramanian <prabhu@appthreat.com>
    
    * Remove debug
    
    Signed-off-by: Prabhu Subramanian <prabhu@appthreat.com>
    
    ---------
    
    Signed-off-by: Prabhu Subramanian <prabhu@appthreat.com>

---
+++ test/data/yarn-workspaces-same-version-demo/packages/app-a/index.js
@@ -0,0 +1,2 @@
+import dayjs from "dayjs";
+console.log("[app-a] dayjs version:", dayjs().format("YYYY-MM-DD"));
--- types/third-party/arborist/lib/tracker.d.ts
+++ types/third-party/arborist/lib/tracker.d.ts
@@ -2,11 +2,11 @@ export default Tracker;
 declare function Tracker(cls: any): {
     new (): {
         [x: string]: any;
-        #progress: Map<any, any>;
-        #createTracker(key: any, name: any): void;
+        "__#private@#progress": Map<any, any>;
+        "__#private@#createTracker"(key: any, name: any): void;
         addTracker(section: any, subsection?: any, key?: any): void;
         finishTracker(section: any, subsection?: any, key?: any): void;
-        #onError(msg: any): void;
+        "__#private@#onError"(msg: any): void;
     };
     [x: string]: any;
 };
